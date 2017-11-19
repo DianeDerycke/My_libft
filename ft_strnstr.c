b657650046d12df1960fcd506cd473167b758175
Diane DERYCKE <dideryck@student.42.fr>
2017-11-19 08:53:13 +0100

    update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,25 +6,27 @@
 /*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 21:15:17 by dideryck          #+#    #+#             */
-/*   Updated: 2017/11/19 03:37:30 by dideryck         ###   ########.fr       */
+/*   Updated: 2017/11/19 08:30:50 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t		i;
-	size_t		n;
+	char	*tmp;
 
-	i = 0;
-	n = 0;
-	n = ft_strlen(little);
-	while (big[i] && i < len)
+	tmp = 0;
+	tmp = ft_strncpy(tmp, big, len);
+	if (tmp =='\0' && little == '\0')
+		return (tmp);
+	while (len)
 	{
-		if (ft_strncmp(big + i, little, n) == 0)
-			return (((char*)big) + i);
-		i++;
+		if (ft_strncmp(tmp, little, ft_strlen(little)) == 0)
+			return (tmp);
+		tmp++;
+		len = len - ft_strlen(little);
 	}
 	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dideryck <dideryck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 19:18:38 by dideryck          #+#    #+#             */
-/*   Updated: 2017/11/16 02:29:11 by dideryck         ###   ########.fr       */
+/*   Updated: 2017/11/19 07:36:29 by dideryck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,13 @@ char	*ft_strrchr(const char *s, int c)
 
 	i = 0;
 	j = 0;
-	if (s == NULL)
-		return (NULL);
 	while (s[j])
 	{
 		if (s[j] == ((char)c))
 			i = j;
 		j++;
 	}
-	if (s[i] == '\0' && c == '\0')
+	if (s[j] == '\0' && c == '\0')
 		return ((char*)(s + j));
 	if (i == 0 && s[i] != ((char)c))
 		return (NULL);
